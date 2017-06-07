a59d0778f4a3320eff8bc74654135bd67b1edeb7
Elger van Boxtel <elgervb@gmail.com>
2017-06-07 21:37:54 +0200

    use pph-app

--- src/index.php
+++ src/index.php
@@ -1,9 +1,29 @@
-<?php 
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+ini_set('html_errors', 'off');
+
+use app\App;
+use validate\Args;
+use handler\json\Json;
+use handler\json\JsonHandler;
+use sysinfo\Sysinfo;
+
 /**
  *
  * @author Elger van Boxtel
  *
  */
-date_default_timezone_set('UTC');
+$app = App::create();
+$app->addHandler(new JsonHandler());
 
-phpinfo();
+$app->route('/info/?([0-9]+)?', function($what = null) {
+	$what = Args::int($what)->value();
+	$what ? phpinfo($what) : phpinfo();
+});
+
+$app->route('/sysinfo', function() {
+	return new Json(Sysinfo::create());
+});
+
+$app->start();
